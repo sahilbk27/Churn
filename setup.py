3784178e6a21bfdb92d35987beac3073f56d3415
Sahil_Khot <khotsahil27@gmail.com>
2026-04-19 23:43:45 +0530

    packages

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPHEN_E_DOT= "_e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="Churn Prediction",
+    version="0.0.1",
+    author="Sahil",
+    author_email="khotsahil27@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
